91aebcd32ceaeac21d785f4aec542e9d6f95bdd4
girlgogo <599067613@qq.com>
2017-05-01 17:53:56 +0800

    加快Vue实例的创建，优化低网速下的页面加载体验

--- html/static/script/pages/index.js
+++ html/static/script/pages/index.js
@@ -3,48 +3,61 @@ import $ from 'jquery'
 import '../../css/reset.css'
 import '../../css/all.css'
 
-$.get('/ajax/index',function (d) {
-	let windowWidth = $(window).width();
-	if (windowWidth < 320) {
-		windowWidth = 320
-	}
-	let index_header_tab_width = $('.Swipe-tab').find('a').eq(0).outerWidth()
-	new Vue({
-		el: '#app',
-		data: {
-			screen_width: windowWidth,
-			double_screen_width: windowWidth*2,
-			index_header_tab_width: index_header_tab_width,
-			top: d.items[0].data.data,
-			hot: d.items[1].data.data,
-			recommend: d.items[2].data.data,
-			female: d.items[3].data.data,
-			male: d.items[4].data.data,
-			free: d.items[5].data.data,
-			topic: d.items[6].data.data,
-			duration: 0,
-			position: 0,
-			header_position: 0,
-			header_duration:0,
-			tab_1_class: 'Swipe-tab__on',
-			tab_2_class: ''
-		},
-		methods: {
-			tabSwitch: function (pos) {
-				this.duration = 0.5
-				this.header_duration = 0.5
-				if (pos == 0) {
-					this.position = 0
-					this.header_position = 0
-					this.tab_1_class = 'Swipe-tab__on'
-					this.tab_2_class = ''
-				} else {
-					this.position = (-windowWidth)
-					this.header_position = index_header_tab_width
-					this.tab_2_class = 'Swipe-tab__on'
-					this.tab_1_class = ''
-				}
+let windowWidth = $(window).width();
+if (windowWidth < 320) {
+	windowWidth = 320
+}
+let index_header_tab_width = $('.Swipe-tab').find('a').eq(0).outerWidth()
+
+const vm = new Vue({
+	el: '#app',
+	data: {
+		screen_width: windowWidth,
+		double_screen_width: windowWidth*2,
+		index_header_tab_width: index_header_tab_width,
+		top: [],
+		hot: [],
+		recommend: [],
+		female: [],
+		male: [],
+		free: [],
+		topic: [],
+		duration: 0,
+		position: 0,
+		header_position: 0,
+		header_duration:0,
+		tab_1_class: 'Swipe-tab__on',
+		tab_2_class: '',
+		a: {
+			b: 1
+		}
+	},
+	created () {
+		$.get('/ajax/index', (d) => {
+			this.top = d.items[0].data.data
+			this.hot = d.items[1].data.data
+			this.recommend = d.items[2].data.data
+			this.female = d.items[3].data.data
+			this.male = d.items[4].data.data
+			this.free = d.items[5].data.data
+		},'json')
+	},
+	methods: {
+		tabSwitch: function (pos) {
+			this.duration = 0.5
+			this.header_duration = 0.5
+			if (pos == 0) {
+				this.position = 0
+				this.header_position = 0
+				this.tab_1_class = 'Swipe-tab__on'
+				this.tab_2_class = ''
+			} else {
+				this.position = (-windowWidth)
+				this.header_position = index_header_tab_width
+				this.tab_2_class = 'Swipe-tab__on'
+				this.tab_1_class = ''
 			}
 		}
-	})
-},'json')
+	}
+})
+
